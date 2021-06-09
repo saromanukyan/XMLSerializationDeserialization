36d356322604704e339596d55d22346ecddb294a
saromanukyan <81385397+saromanukyan@users.noreply.github.com>
2021-06-10 01:37:35 +0400

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class Test {
     public static void main(String[] args) {
 
-        Cocktail cocktail = new Cocktail("Mudslide", 250.69, true);
+         Cocktail cocktail1 = new Cocktail("Mudslide", 250.69, true);
 
         List<Ingredient> ingredients = new ArrayList<>();
         ingredients.add(new Ingredient("Dark chocolate", 50));
@@ -13,13 +13,13 @@ public class Test {
         ingredients.add(new Ingredient("Irish cream liqueur", 60));
         ingredients.add(new Ingredient("Double cream", 100));
 
-        cocktail.setIngredients(ingredients);
+        cocktail1.setIngredients(ingredients);
 
-        String xml = CocktailService.serializeXML(cocktail);
+        String xml = CocktailService.serializeXML(cocktail1);
         System.out.println(xml);
 
-        System.out.println(CocktailService.deserializeXML(xml));
-
+        Cocktail cocktail2=CocktailService.deserializeXML(xml);
+        System.out.println(cocktail2);
     }
 
 }
